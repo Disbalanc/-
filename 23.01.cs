999c44cebd5c2f1a10dec115075f3e22d6745412
Disbalanc <103411764+Disbalanc@users.noreply.github.com>
2023-01-23 22:58:27 +0500

    Update 23.01.cs

--- 23.01.cs
+++ 23.01.cs
@@ -1,41 +1,36 @@
 using static System.Net.Mime.MediaTypeNames;
-
 internal class TV
 {
     public int currentChannel = 1;
     public int channelLimit = 10;
     public TV()
     {
-        string answer;
+        int answer;
         Console.WriteLine($"Лимит каналов: {channelLimit}");
         do
         {
             Console.WriteLine($"Текущий канал: {currentChannel}");
             Console.WriteLine($"\n\t1 - следующий канал!\n\t2 - предыдущий канал!\n\t3 - выбора канала по числу!\n\t4 - выход из программы");
             Console.Write("Ваш выбор: ");
-            answer = Console.ReadLine().ToLower().Trim();
-            switch (answer)
+            
+            if (!int.TryParse(Console.ReadLine().ToLower().Trim(), out answer))
             {
-                case "1":
-                    {
-                        NextChannel();
-                        break;
-                    }
-                case "2":
-                    {
-                        PreviousChannel();
-                        break;
-                    }
-                case "3":
-                    {
-                        Console.Write("Введите число: ");
-                        int numberOfChannel = Convert.ToInt32(Console.ReadLine());
-                        MoveToChannel(numberOfChannel);
-                        break;
-                    }
+                Console.WriteLine("\nОшибка! не-то что-то ввели");
+            }
+            if (answer == 1)
+                NextChannel();
+            else if (answer == 2)
+            {
+                PreviousChannel();
+            }
+            else if (answer == 3)
+            {
+                Console.Write("Введите число: ");
+                int numberOfChannel = Convert.ToInt32(Console.ReadLine());
+                MoveToChannel(numberOfChannel);
             }
         }
-        while (answer != "4");
+        while (answer != 4);
     }
     public void NextChannel()
     {
